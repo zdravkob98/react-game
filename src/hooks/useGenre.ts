e662551c5043433ad282d13b807fa968d8701c64
Zdravko Bonev <zdravko.bonev@era.io>
2023-05-02 14:41:53 +0300

    Fetch the Genres

---
+++ src/hooks/useGenre.ts
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Genre {
+  id: number
+  count: number
+  name: string
+}
+
+interface FetchGenresResponse {
+  count: number
+  results: Genre[]
+}
+
+const useGenres = () => {
+  const [genres, setGenres] = useState<Genre[]>([]);  
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController()
+    setLoading(true)
+    apiClient
+      .get<FetchGenresResponse>("/genres", { signal: controller.signal })
+      .then((res) => {
+        setLoading(false)
+        setGenres(res.data.results)}
+      )
+      .catch((err) => {
+        if (err instanceof CanceledError) return
+        setLoading(false)
+        setError(err.message)
+      }
+      );
+
+    return() => controller.abort();
+  }, []);
+
+  return { genres, error, loading }; 
+}
+
+
+export default useGenres
