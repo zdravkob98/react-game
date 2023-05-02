1a2e5ba9f8f03ef8b974e116dc168739f0a943ab
Zdravko Bonev <zdravko.bonev@era.io>
2023-05-02 15:47:07 +0300

    Filter games by genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,9 +3,14 @@ import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
+import { Genre } from "../hooks/useGenre";
 
-const GameGrid = () => {
-  const { data, error, loading } = useGames();
+interface Props {
+  selectedGenre: Genre | null;
+}
+
+const GameGrid = ({ selectedGenre }: Props) => {
+  const { data, error, loading } = useGames(selectedGenre);
   const skeletos = [1, 2, 3, 4, 5, 6];
 
   return (
@@ -18,13 +23,13 @@ const GameGrid = () => {
       >
         {loading &&
           skeletos.map((skeleton) => (
-            <GameCardContainer>
-              <GameCardSkeleton key={skeleton} />
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton />
             </GameCardContainer>
           ))}
         {data.map((game) => (
-          <GameCardContainer>
-            <GameCard key={game.id} game={game} />
+          <GameCardContainer key={game.id}>
+            <GameCard game={game} />
           </GameCardContainer>
         ))}
       </SimpleGrid>
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,38 +1,47 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 
 interface FetchResponse<T> {
-  count: number
-  results: T[]
+  count: number;
+  results: T[];
 }
 
-const useData = <T>(endpoint: string) => {
-  const [data, setData] = useState<T[]>([]);  
+const useData = <T>(
+  endpoint: string,
+  requestsConfig?: AxiosRequestConfig,
+  dependencies?: any
+) => {
+  const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const controller = new AbortController()
-    setLoading(true)
-    apiClient
-      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
-      .then((res) => {
-        setLoading(false)
-        setData(res.data.results)}
-      )
-      .catch((err) => {
-        if (err instanceof CanceledError) return
-        setLoading(false)
-        setError(err.message)
-      }
-      );
+  useEffect(
+    () => {
+      const controller = new AbortController();
+      setLoading(true);
+      apiClient
+        .get<FetchResponse<T>>(endpoint, {
+          signal: controller.signal,
+          ...requestsConfig,
+        })
+        .then((res) => {
+          setLoading(false);
+          setData(res.data.results);
+        })
+        .catch((err) => {
+          if (err instanceof CanceledError) return;
+          setLoading(false);
+          setError(err.message);
+        });
 
-    return() => controller.abort();
-  }, []);
+      return () => controller.abort();
+    },
+    dependencies ? [...dependencies] : []
+  );
+  console.log(dependencies ? [...dependencies] : []);
 
-  return { data, error, loading }; 
-}
+  return { data, error, loading };
+};
 
-
-export default useData
+export default useData;
