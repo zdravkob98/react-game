a25aef41ef947865c7695a5247593dc59b95cf1d
Zdravko Bonev <zdravko.bonev@era.io>
2023-05-08 18:27:40 +0300

    Add expandeble text

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,21 +1,22 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, Spinner } from "@chakra-ui/react";
+import ExpandableText from "../components/ExpandableText";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
 
   if (isLoading) {
-    <Spinner />;
+    return <Spinner />;
   }
 
-  if (error) throw error;
+  if (error || !game) throw error;
 
   return (
     <>
-      <Heading>{game?.name}</Heading>
-      <Text>{game?.description_raw}</Text>
+      <Heading>{game.name}</Heading>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
