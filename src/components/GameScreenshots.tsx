456f27f10cc9989e7f895a049b93878f730d4494
Zdravko Bonev <zdravko.bonev@era.io>
2023-05-08 19:55:46 +0300

    Build Game Screenshots

---
+++ src/components/GameScreenshots.tsx
@@ -0,0 +1,22 @@
+import { Image, SimpleGrid } from "@chakra-ui/react";
+import useScreenshots from "../hooks/useScreenshots";
+
+interface Props {
+  gameId: number;
+}
+
+const GameScreenshots = ({ gameId }: Props) => {
+  const { data, isLoading, error } = useScreenshots(gameId);
+  if (error) throw error;
+  if (isLoading) return null;
+
+  return (
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2}>
+      {data?.results.map((file) => (
+        <Image key={file.id} src={file.image} />
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default GameScreenshots;
---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { Trailer } from "../entities/Trailer";
+import APIClient from "../services/api-client";
+import { Screenshots } from "../entities/Screenshots";
+
+const useScreenshots = (gameId: number) => {
+  const apiClient = new APIClient<Screenshots>(`/games/${gameId}/screenshots`);
+  return useQuery({
+    queryKey: ["screenshots", gameId],
+    queryFn: apiClient.getAll,
+  });
+};
+
+export default useScreenshots;
